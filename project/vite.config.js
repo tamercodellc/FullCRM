213f4033aef802c3f6cc87b1e4f3a62f23a55c84
tamercodellc <152832864+tamercodellc@users.noreply.github.com>
2025-05-08 17:39:39 -0400

    Start development server

--- project/vite.config.js
+++ project/vite.config.js
@@ -4,7 +4,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    port: 5173
+    port: 5173,
+    host: true,
+    hmr: {
+      clientPort: 443
+    }
   },
   plugins: [react()],
   resolve: {
